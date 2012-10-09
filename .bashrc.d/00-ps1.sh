bc0bf3d0ef9881bd53005c529dc745841e407c05
Ævar Arnfjörð Bjarmason <avar@booking.com>
2012-10-09 10:25:38 +0200

    .bashrc.d/: use git-prompt.bash, delete git-completion.bash
    See https://github.com/git/git/commit/af31a45, this was split up by
    upstream.

--- .bashrc.d/00-ps1.sh
+++ .bashrc.d/00-ps1.sh
@@ -37,10 +37,8 @@ function dir_info() {
     fi
 
     if type git >&/dev/null; then
-        if test -z "$(type -t __git_ps1)"; then
-            # Include my copy of the git-completion.bash
-            . ~/.bashrc.d/contrib/git-completion.bash
-        fi
+        # Include my copy of the git-prompt.bash
+        . ~/.bashrc.d/contrib/git-prompt.bash
 
         if test -n "$(type -t __git_ps1)"; then
             # We can hopefully use __git_ps1 which comes with git's
