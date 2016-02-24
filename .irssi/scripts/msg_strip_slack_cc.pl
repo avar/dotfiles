5cc09e94492303d9fca73b68d6a0e1e5500f7697
Ævar Arnfjörð Bjarmason <avar@booking.com>
2016-02-24 16:29:13 +0100

    Add a script to strip slack @channel mentions
    When someone types @channel on slack then the IRC gateway will
    auto-add " [cc: you]" to the end of the message, which causes a
    hilight. Disable this. if I want hilights like this I can hilight on
    "@channel".

---
+++ .irssi/scripts/msg_strip_slack_cc.pl
@@ -0,0 +1,44 @@
+use strict;
+use warnings;
+use Irssi;
+
+our $VERSION = "1";
+our %IRSSI = (
+    authors     => 'Ævar Arnfjörð Bjarmason',
+    contact     => 'avarab@gmail.com',
+    name        => 'Msg strip slack CC',
+    description => 'Strips the annoying mentions of your nickname via [cc: <you>]',
+    license     => 'Public Domain',
+    url         => 'http://irssi.org/',
+);
+
+# HOWTO:
+#
+#   /load msg_strip_slack_cc.pl
+#
+# This should just automatically work, it'll detect if you're using
+# the slack IRC gateway and auto-detect the nick it should be
+# stripping as well.
+
+use constant {
+    I_SERVER => 0,
+    I_DATA   => 1,
+    I_NICK   => 2,
+    I_MASK   => 3,
+    I_TARGET => 4,
+};
+
+sub rewrite_privmsg {
+    my ($server, $data, $nick, $nick_and_address) = @_;
+    my ($target, $message) = split /:/, $data, 2;
+
+    return unless $server->{address} =~ /irc\.slack\.com$/s;
+    my $wanted_nick = $server->{wanted_nick};
+    return unless $message =~ s/ \[cc: \Q$wanted_nick\E\]$//s;
+
+    my $new_data = "$target:$message";
+    Irssi::signal_emit('event privmsg', $server, $new_data, $nick, $nick_and_address);
+    Irssi::signal_stop();
+}
+
+Irssi::signal_add('event privmsg', 'rewrite_privmsg');
