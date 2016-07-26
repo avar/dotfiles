f4b84ea849c578f5e841eef5a15cdb844f7ecd06
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2016-07-26 21:19:12 +0200

    offlineimap: An (undocumented) hack to call a function in .offlineimap.py

--- .offlineimap.py
+++ .offlineimap.py
@@ -1,6 +1,7 @@
 # This does tilde (~) expansion which python's open() doesn't do
 # natively.
 from os.path import expanduser
+import re
 
 def get_password(what):
     path = expanduser("~/.password/" + what)
@@ -10,3 +11,19 @@ def get_password(what):
     fh.close()
 
     return content
+
+# Any rule added to one of these subs needs an inverse rule in the
+# other one.
+def oi_work_local_nametrans(name):
+    name = re.sub('^Sent$',  'Sent Items',    name)
+    name = re.sub('^Trash$', 'Deleted Items', name)
+    name = re.sub('^Junk$',  'Junk E-Mail',   name)
+
+    return name
+
+def oi_work_remote_nametrans(name):
+    name = re.sub('^Sent Items$',    'Sent',  name)
+    name = re.sub('^Deleted Items$', 'Trash', name)
+    name = re.sub('^Junk E-Mail$',   'Junk',  name)
+
+    return name
