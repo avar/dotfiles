35d41515fbbb58479adf4a87b5f0503a7646170c
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2020-11-09 10:23:03 +0100

    more gdk setup

--- .bashrc.d/11-gitlab-development-kit.sh
+++ .bashrc.d/11-gitlab-development-kit.sh
@@ -5,3 +5,16 @@ then
     # Install the GDK completion
     . ~/g.gl/gitlab-development-kit/support/completions/gdk.bash
 fi
+
+# Add rbenv
+if which rbenv >/dev/null
+then
+    eval "$(rbenv init -)"
+fi
+
+maybe_add_path /usr/lib/go-1.14/bin
+
+# nvm, for nodejs etc.: https://github.com/nvm-sh/nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
