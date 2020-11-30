516d9dfa2ab86d0663ff8cb5c5ee9a522377434d
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2020-11-30 11:11:27 +0100

    offlineimap/mu4e: attempt to migrate away from different DB per acount + setup new account
    Setup new work account, and attempt to migrate this whole thing.
    
    See avar/dotemacs@7d23a13 (mu4e: migrate to one-database setup,
    2020-11-30) for the emacs side of this.

--- .offlineimap.py
+++ .offlineimap.py
@@ -12,8 +12,7 @@ def get_password(what):
 
     return content
 
-# This is a superset of the more limited work rules
-def oi_personal_gmail_local_nametrans(name):
+def oi_gmail_local_namentrans(name):
     name = re.sub('^Drafts$', '[Gmail]/Drafts',    name)
     name = re.sub('^Sent$',   '[Gmail]/Sent Mail', name)
     name = re.sub('^Junk$',   '[Gmail]/Spam',      name)
@@ -21,10 +20,31 @@ def oi_personal_gmail_local_nametrans(name):
 
     return name
 
-def oi_personal_gmail_remote_nametrans(name):
+def oi_remote_nametrans(name):
     name = re.sub('^\[Gmail\]/Drafts$',    'Drafts', name)
     name = re.sub('^\[Gmail\]/Sent Mail$', 'Sent',   name)
     name = re.sub('^\[Gmail\]/Spam$',      'Junk',   name)
     name = re.sub('^\[Gmail\]/Trash$',     'Trash',  name)
 
     return name
+
+# This is a superset of the more limited general rules
+def oi_personal_gmail_local_nametrans(name):
+    name = oi_gmail_local_namentrans(name)
+    return name
+
+
+def oi_personal_gmail_remote_nametrans(name):
+    name = oi_remote_nametrans(name)
+    return name
+
+# This is a superset of the more limited general rules
+def oi_gitlab_gmail_local_nametrans(name):
+    name = oi_gmail_local_namentrans(name)
+    return name
+
+
+def oi_gitlab_gmail_remote_nametrans(name):
+    name = oi_remote_nametrans(name)
+    return name
+
