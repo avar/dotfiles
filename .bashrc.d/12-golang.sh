ba8cd5b0826d022136948ec5d2c563ebbf99b5a3
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2020-12-18 15:13:37 +0100

    gitlab screen: move out GOMAXPROCS= & add git alias for testing tag/branch

--- .bashrc.d/12-golang.sh
+++ .bashrc.d/12-golang.sh
@@ -4,4 +4,9 @@ if type go >/dev/null 2>&1
 then
     export GOPATH=$HOME/gocode
     export PATH=$PATH:$GOPATH/bin
+
+    if type parallel >/dev/null 2>&1
+    then
+        export GOMAXPROCS=$(parallel --number-of-cores)
+    fi
 fi
