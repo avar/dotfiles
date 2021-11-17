cd1651e800b91eda0cd4c14950a81abdd58fbeea
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-11-17 16:36:23 +0100

    .bashrc / screen: use MAKEOPTS/HARNESS_OPTIONS instead of -j everywhere

--- .bashrc.d/00-variables.sh
+++ .bashrc.d/00-variables.sh
@@ -1,2 +1,11 @@
 ## Variables I use in the rest of ~/.bashrc.d and ~/.aliases.d
 export NPROC=$(nproc)
+
+## Concurrency
+# go(1)
+export GOMAXPROCS=$NPROC
+# make(1)
+export MAKEFLAGS=j$NPROC
+# prove(1)
+export HARNESS_OPTIONS=j$NPROC
+
--- .bashrc.d/12-golang.sh
+++ .bashrc.d/12-golang.sh
@@ -5,11 +5,6 @@ then
     export GOPATH=$HOME/gocode
     export PATH=$PATH:$GOPATH/bin
 
-    if type parallel >/dev/null 2>&1
-    then
-        export GOMAXPROCS=$NPROC
-    fi
-
     if test -d /usr/lib/go-1.16/bin
     then
 	export PATH=/usr/lib/go-1.16/bin:$PATH
