avar/dotfiles

commit c3207fa04c61094da07b3dc9b5b4574b3f17405c

.bashrc: don't make ASAN not detect leaks...

.bashrc
@@ -83,9 +83,10 @@ maybe_add_path $HOME/local/bin
 
 # Only if the screen configuration (dsgit) added it already
 move_up_in_path $HOME/g/git/bin-wrappers
-# If I'm running with ASAN (for git) let's not die on memory leaks,
-# just memory misuse
-export ASAN_OPTIONS=detect_leaks=0
+
+# Use less shitty stack traces in LSAN at the cost of a bit of
+# (sometimes ~2-10%) slowdown.
+export LSAN_OPTIONS=fast_unwind_on_malloc=0
 
 # Custom commands
 maybe_add_path $HOME/g/hyperfine/target/debug

commit 1c530d8058a4b6199ce2a8fe9ad0209f02f9dd41

.bashrc: fix up git test suite breakage in c3207fa
Due to my c3207fa (.bashrc: don't make ASAN not detect leaks...,
2022-01-17) and an outstanding issue in test-lib.sh (haven't submitted
the patch yet) it'll ignore its own abort_on_error=1 in this case.

.bashrc
@@ -100,7 +100,7 @@ move_up_in_path $HOME/g/git/bin-wrappers
 
 # Use less shitty stack traces in LSAN at the cost of a bit of
 # (sometimes ~2-10%) slowdown.
-export LSAN_OPTIONS=fast_unwind_on_malloc=0
+export LSAN_OPTIONS=fast_unwind_on_malloc=0:abort_on_error=1
 
 # Custom commands
 maybe_add_path $HOME/g/hyperfine/target/debug