553547d398c3a5e8e81cc312ab6b71ea13ba2578
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2022-03-21 14:16:02 +0100

    mail setup: tried & failed to get public inbox v2 format working
    It stalled for some reason, so ~/g/git-ml had
    e.g. pull.1182.git.1647845516517.gitgitgadget@gmail.com, but despite
    running the update I didn't get it in the ~/Maildir (nevermind in "mu
    find").
    
    After this whole failing exercise I nuked the whole setup, re-indexed
    etc, and now have it:
    
        $ grep -r ^Message.*pull.1182.git.1647845516517.gitgitgadget@gmail.com *
        lei-q-git-ml/cur/0e51c3944f904150d11eb11627f61e440a448d9a=99:2,:Message-Id: <pull.1182.git.1647845516517.gitgitgadget@gmail.com>
    
    And, instead of:
    
        $ mu find msgid:pull.1182.git.1647845516517.gitgitgadget@gmail.com
        error: no matches for search expression
    
    I get:
    
        $ mu find msgid:pull.1182.git.1647845516517.gitgitgadget@gmail.com
        Mon 21 Mar 2022 07:51:56 AM CET Tao Klerks via GitGitGadget <gitgitgadget@gmail.com> [PATCH] t3200: fix antipatterns in existing branch tests

--- .bashrc.d/20-public-inbox-lei.sh
+++ .bashrc.d/20-public-inbox-lei.sh
@@ -2,10 +2,18 @@
 
 PU=$HOME/g/public-inbox.org
 if test -d $PU
-then
-	export PERL5LIB=$PU/blib/lib:$PU/Documentation
-	maybe_add_path $PU/blib/script
+then	. $PU/contrib/completion/lei-completion.bash
 
-	. $PU/contrib/completion/lei-completion.bash
+	# To avoid buildig the software in blib/, makes debugging
+	# easier.
+	lei () {
+		perl -I $PU/lib $PU/script/lei "$@"
+	}
+
+	public-inbox () {
+		cmd=$1 &&
+		shift &&
+		perl -I $PU/lib $PU/script/public-inbox-$cmd "$@"
+	}
 fi
 
